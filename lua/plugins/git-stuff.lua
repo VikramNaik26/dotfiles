839e1ff479d2ce947b1b604c54a753129542942a
Vikram Naik <26vikramnaik@gmail.com>
2024-04-08 11:27:09 +0530

    Added git command shortcuts

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,6 +1,17 @@
 return {
 	{
 		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gad", ":Git add .<CR>", {})
+			vim.keymap.set("n", "<leader>gss", ":Git push<CR>", {})
+
+			_G.git_commit_with_message = function()
+				local commit_message = vim.fn.input("Commit message: ")
+				vim.fn.execute(':Git commit -m "' .. commit_message .. '"')
+			end
+
+			vim.keymap.set("n", "<leader>gcm", ":lua git_commit_with_message()<CR>", {})
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",

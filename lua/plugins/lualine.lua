d683404e5f0f4344ab7b84d0162bbf48ecf6829b
Vikram Naik <26vikramnaik@gmail.com>
2024-04-22 22:14:02 +0530

    fix: Telescope error and lualine buffer file visibility

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,34 +1,32 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin-mocha",
-        sections = {
-          lualine_a = {
-            file = 1,
-          },
-        },
-      },
-      options = {
-        icons_enabled = true,
-        component_separators = "|",
-        section_separators = "",
-      },
-      sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-        },
-        lualine_a = {
-          {
-            "buffers",
-          },
-        },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				component_separators = "|",
+				section_separators = "",
+				theme = "catppuccin-mocha",
+				sections = {
+					lualine_a = {
+						file = 1,
+					},
+				},
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_a = {
+					{
+						"buffers",
+					},
+				},
+			},
+		})
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -33,3 +33,7 @@ vim.cmd("highlight! HarpoonActive guibg=NONE guifg=white")
 vim.cmd("highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7")
 vim.cmd("highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7")
 vim.cmd("highlight! TabLineFill guibg=NONE guifg=white")
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })

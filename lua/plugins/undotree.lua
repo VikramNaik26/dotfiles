aa4f39ed2f880a87b5254c12cd6dd9cde32c170e
Vikram Naik <26vikramnaik@gmail.com>
2024-08-23 00:14:51 +0530

    feat: add undotree to trace changes

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,7 @@
+return {
+  "mbbill/undotree",
+  config = function()
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.api.nvim_set_keymap("n", "<leader>u", ":UndotreeToggle<CR>", { noremap = true, silent = true })
+  end,
+}
